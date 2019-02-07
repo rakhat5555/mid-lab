4244e5ec2f7f20dd054b9487b95087fee31dc6f7
rakhat5555 <rakhat5555@gmail.com>
2019-02-07 14:12:45 -0800

    add method toUpperCase

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,17 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event){
-var person1Input = $("input#person1").val();
-var person2Input = $("input#person2").val();
+      event.preventDefault();
+var person1Input1 = $("input#person1").val();
+var person1Input = person1Input1.toUpperCase();
+var person2Input2 = $("input#person2").val();
+var person2Input = person2Input2.toUpperCase();
 var animalInput = $("input#animal").val();
 
-
-    $(".person1").text(person1Input);
+$(".person1").text(person1Input);
 $(".person2").text(person2Input)
 $(".animal").text(animalInput).val();
 
     $("#story").show();
-    event.preventDefault();
+
   });
 });
