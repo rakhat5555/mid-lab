a3ba521b4594b49b41d475400c79b57c287ca28b
rakhat5555 <rakhat5555@gmail.com>
2019-02-27 16:34:41 -0800

    add arrays aand loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,23 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event){
       event.preventDefault();
-var person1Input1 = $("input#person1").val();
-var person1Input = person1Input1.toUpperCase();
-var person2Input2 = $("input#person2").val();
-var person2Input = person2Input2.toUpperCase();
-var animalInput = $("input#animal").val();
+ var blanks = ["person1", "person2", "animal", "exlamination", "verb", "noun"];
+ blanks.forEach(function(blank) {
+   var userInput = $("input#" + blank).val();
+   $("." + blank).text(userInput);
+ });
 
-$(".person1").text(person1Input);
-$(".person2").text(person2Input)
-$(".animal").text(animalInput).val();
+
+
+// var person1Input1 = $("input#person1").val();
+// var person1Input = person1Input1.toUpperCase();
+// var person2Input2 = $("input#person2").val();
+// var person2Input = person2Input2.toUpperCase();
+// var animalInput = $("input#animal").val();
+//
+// $(".person1").text(person1Input);
+// $(".person2").text(person2Input)
+// $(".animal").text(animalInput).val();
 
     $("#story").show();
 
